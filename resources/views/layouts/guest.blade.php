b14bf487088167d966fb0d52f0bb51c33da8cd69
ecogalileo <ericogalileo@gmail.com>
2023-01-26 14:50:44 +0800

    resize the logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,15 +15,21 @@
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-            <div>
+            {{-- <div>
                 <a href="/">
-                    <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
+                    <x-application-logo class="w-64 h-64 fill-current text-gray-500" />
                 </a>
-            </div>
+            </div> --}}
 
             <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+                <div class="grid  place-content-center">
+                    <a href="/">
+                        <x-application-logo class="w-48 h-48 fill-current text-gray-500" />
+                    </a>
+                </div>
                 {{ $slot }}
             </div>
+
         </div>
     </body>
 </html>
